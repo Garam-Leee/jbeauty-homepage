4f3105efbc7d653823eecaa010969e706325f91f
front-lucy <devfront@jaedam.com>
2026-01-22 18:46:17 +0900

    init: nextjs + ts + emotion

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+cat > babel.config.js <<'EOF'
+module.exports = {
+  presets: ["next/babel"],
+  plugins: [["@emotion/babel-plugin", { sourceMap: true, autoLabel: "dev-only", labelFormat: "[local]" }]],
+};
+EOF
